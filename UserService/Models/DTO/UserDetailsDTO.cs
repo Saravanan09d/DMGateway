b9d41eecb0e736b4c1ea618af1e6b6580f18959a
Saravanan09d <111735144+Saravanan09d@users.noreply.github.com>
2023-12-12 16:32:05 +0530

    user details

---
+++ UserService/Models/DTO/UserDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace UserService.Models.DTO
+{
+    public class UserDetailsDTO
+    {
+        //public int Id { get; set; }
+        //public string Email { get; set; }
+        //public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string Token { get; set; }
+    }
+
+}
